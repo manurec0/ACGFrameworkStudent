79851774326c9e15fdf2d3f16061556171ddbf4b
eduardvalles <eduard.valles01@estudiant.upf.edu>
2024-11-07 23:45:19 +0100

    Update volume.fs
    Absorption ya funciona, solo falta color.

--- res/shaders/volume.fs
+++ res/shaders/volume.fs
@@ -1,8 +1,11 @@
 #version 330 core
 
-uniform float u_start_position;    // Starting position (ta)
-uniform float u_ending_position;    // Ending position (tb)
+in vec3 v_world_position;
 
+//uniform float u_start_position;    // Starting position (ta)
+//uniform float u_ending_position;    // Ending position (tb)
+
+uniform mat4 u_model;
 uniform vec3 u_camera_position;
 uniform vec3 u_color;
 uniform vec4 u_ambient_color;
@@ -11,7 +14,7 @@ uniform float u_absorption_coefficient;
 uniform vec3 u_boxMin;
 uniform vec3 u_boxMax;
 
-
+uniform int u_volume_type;
 
 
 out vec4 FragColor;
@@ -28,17 +31,37 @@ vec2 intersectAABB(vec3 rayOrigin, vec3 rayDir, vec3 boxMin, vec3 boxMax) {
 
 void main()
 {
+    mat4 inverseModel = inverse(u_model);
+    vec4 temp = vec4(u_camera_position, 1.0);
+    temp *= inverseModel;
+    vec3 local_camera_pos = vec3(temp.x / temp.w, temp.y / temp.w, temp.z / temp.w);
+
+    // Compute ray direction
+    vec3 rWorld = normalize(v_world_position - u_camera_position);
+    vec3 r = normalize((inverseModel * vec4(rWorld, 0.0)).xyz);
+
+    vec3 boxMin = vec3(-1.0, -1.0, -1.0);
+    vec3 boxMax = vec3(1.0, 1.0, 1.0);
+
+    vec2 intersection = intersectAABB(local_camera_pos, r, boxMin, boxMax);
+
+    float ta = intersection.x;
+    float tb = intersection.y;
+
     // Calculate the optical thickness
     vec3 B = vec3(u_background_color.x, u_background_color.y, u_background_color.z);
-    float opticalThickness = u_absorption_coefficient * (u_ending_position - u_start_position);
+    //float opticalThickness = u_absorption_coefficient * (u_ending_position - u_start_position);
+    float opticalThickness = tb - ta;
 
     // Compute transmittance using Beer-Lambert Law
-    float transmittance = exp(-opticalThickness);
+    float transmittance = exp(-u_absorption_coefficient * opticalThickness);
 
     // Final color blending with background color
-    vec4 volumeColor = u_ambient_color * transmittance;
-    //vec4 finalColor = mix(u_background_color, volumeColor, transmittance);
     vec3 finalColor = B * transmittance;
 
     FragColor = vec4(finalColor, 1.0);
+
+
+
+
 }
